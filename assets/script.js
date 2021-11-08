1d99d549525e028ad067e9d44233ff35468c8cee
Cameron Quicksall <cameron.quicksall70@gmail.com>
2021-11-07 20:24:49 -0600

    updated variable logs/saving

--- assets/script.js
+++ assets/script.js
@@ -7,45 +7,48 @@ var generateBtn = document.querySelector("#generate");
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var upperCase = ["Q", "W", "E","R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
 var lowerCase = ["q","w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
-var special = ["!", "@", "#", "$", "%", "&", "?", "+", "-"];
+var special = ["!", "@", "#", "$", "%", "&", "?", "+", "-", "^", "(", ")", "=", ";", ":", "/", "~"];
 
 function generatePassword() {
 
-    //User prompted for criteria
-var length = window.prompt("Please choose a number between 8-128 characters");      
-    //letters
+    //User prompted for length of password
+var length = window.prompt("Please choose a number between 8-128 characters");
+console.log("A length was chosen");
+    // Lowercase
 var confirmLower = window.confirm("Would like to include lower case letters?");
-        // letters
+console.log("Lowercase Letters Selected");
+        // Uppercase
 var confirmUpper = window.confirm("Would you like to include capital letters?");
-            // numbers
+console.log("Capital Letters Selected");
+            // Numbers
 var confirmNumber = window.confirm("Would you like to include numbers?");
-                    // special characters
+console.log("Numbers Selected");
+                    // Special Characters
 var confirmSpec = window.confirm("Would you like to include special letters / symbols?");
-
+console.log("Symbols and/or Special Character Selected");
 var makepwd = [];
 
 if (length <8) {
     window.alert("Please try again. This is too short");
 } else if (length > 128) {
     window.alert("Please try again. This is too long");
-} else
+} else 
 
 if (confirmLower === true) {
-    makepwd = makepwd.concat(confirmLower);
+    makepwd = makepwd.concat(lowerCase);
 }
 if (confirmUpper === true) {
-    makepwd = makepwd.concat(confirmUpper);
+    makepwd = makepwd.concat(upperCase);
 }
 if (confirmNumber === true) {
-    makepwd = makepwd.concat(confirmNumber);
+    makepwd = makepwd.concat(numbers);
 }
 if (confirmSpec === true) {
-    makepwd = makepwd.concat(confirmSpec);
+    makepwd = makepwd.concat(special);
     }
 
 if (confirmLower === false && confirmUpper === false && confirmNumber === false && confirmSpec === false) {
         window.alert("You must confirm whether or not you want to include the selections above");
-        return;
     }
         console.log(makepwd);
 
@@ -55,8 +58,9 @@ for(var i = 0; i < length; i++) {
     console.log();
     generateRandom.push(random);
 }
-return generateRandom.join("");
+        return generateRandom.join('');
 }
+
 
 // Write password to the #password input
     function writePassword() {
